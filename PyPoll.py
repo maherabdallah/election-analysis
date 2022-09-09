a9e2a50fb8f20877c03294babf301c2a1a5c1444
Maher Abdallah <maher.abdallah98@gmail.com>
2022-09-09 19:16:21 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -83,5 +83,3 @@ winning_candidate_summary = (
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
 print(winning_candidate_summary)
-# Print the candidate list.
-# print(candidate_votes)
